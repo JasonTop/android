f64f58b0443a4aeb693fcfd70fa415a005d12dee
Jason <Jasonleehaowei@gmail.com>
2025-04-02 14:38:09 +0800

    feat: 調整生產環境

---
+++ MyWebViewTest/app/src/main/java/com/example/mywebviewtest/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mywebviewtest
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.requestFocus()
+        webView.requestFocusFromTouch()
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+
+        webView.loadUrl("https://test001.zqyp70.com/en")
+    }
+}
